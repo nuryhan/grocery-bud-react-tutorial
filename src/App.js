7b18a7d53e405ee33729c959c94856a034f2da2b
nuryhan <nurmuhammet.muradov0309@gmail.com>
2021-11-06 17:30:31 +0500

    showAlert and clearList functions

--- src/App.js
+++ src/App.js
@@ -22,16 +22,25 @@ function App() {
     }
     else {
       // show alert
+      setAlert({show: true, msg: 'item added to the list', type: 'success' })
       const newItem = {id: new Date().getTime().toString(), title: name};
       setList([...list, newItem])
       setName('');
     }
   }
 
+  const showAlert =(show = false, type ='', msg = '')=>{
+    setAlert({show,type,msg});
+  }
+
+  const clearList = ()=> {
+    setAlert({show: true, msg: 'empty list', type: 'danger' })
+    setList([]);
+  }
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit} >
-        {alert.show && <Alert {...alert} />}
+        {alert.show && <Alert {...alert} removeAlert={showAlert} />}
         <h3>Grocery bud </h3>
         <div className="form-control">
           <input type="text" className="grocery" placeholder="e. g. eggs" value={name} 
@@ -43,7 +52,7 @@ function App() {
       </form>
       {list.length > 0  && (<div className="grocery-container">
         <List items={list} />
-        <button className="clear-btn">clear items</button>
+        <button className="clear-btn" onClick={clearList} >clear items</button>
       </div>)}
     </section>
   );
