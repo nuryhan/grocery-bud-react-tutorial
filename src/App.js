c7baa8a9344bd4413c80343a22059db8c9c3d79c
nuryhan <nurmuhammet.muradov0309@gmail.com>
2021-11-06 12:12:34 +0500

    alert component complete

--- src/App.js
+++ src/App.js
@@ -7,12 +7,16 @@ function App() {
   const [list, setList] = useState([]);
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID]= useState(null);
-  const [alert,setAlert]= useState({show: false, msg: '', type: ''});
+  const [alert,setAlert]= useState({
+    show: false, 
+    msg: '', 
+    type: ''});
 
   const handleSubmit=(e)=>{
     e.preventDefault();
     if (!name) {
       // display alert
+      setAlert({show: true, msg: 'please enter value', type: 'danger' })
     } else if (name && isEditing) {
       // deal with edit
     }
@@ -27,7 +31,7 @@ function App() {
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit} >
-        {alert.show && <Alert />}
+        {alert.show && <Alert {...alert} />}
         <h3>Grocery bud </h3>
         <div className="form-control">
           <input type="text" className="grocery" placeholder="e. g. eggs" value={name} 
@@ -37,10 +41,10 @@ function App() {
           </button>
         </div>
       </form>
-      <div className="grocery-container">
+      {list.length > 0  && (<div className="grocery-container">
         <List items={list} />
         <button className="clear-btn">clear items</button>
-      </div>
+      </div>)}
     </section>
   );
 }
